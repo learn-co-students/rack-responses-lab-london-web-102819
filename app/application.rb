f3b50e9d6d76384710bfc193ae08b850d66b1333
PianoFF <Fangfei1star@gmail.com>
2019-11-17 16:28:23 +0000

    Done.

---
+++ app/application.rb
@@ -0,0 +1,19 @@
+require "pry"
+class Application
+
+    def call(env)
+        resp = Rack::Response.new
+        greet_1 = "Good Morning!"
+        greet_2 = "Good Afternoon!"
+        
+        t = Time.now
+        # binding.pry
+        puts t
+        if t.hour >= 00 && t.hour<12
+            resp.write greet_1
+        else 
+            resp.write greet_2
+        end
+        resp.finish
+    end
+end
